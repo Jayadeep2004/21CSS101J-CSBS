cf0bd99c7dbd29e411e639833322aaf36950391c
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-09 23:31:23 +0530

    Create Swapping example.c

---
+++ Swapping example.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+ int main()    
+{    
+int a=10, b=20, temp;      
+printf("Before swap a=%d b=%d",a,b);      
+temp = a;
+a = b;
+b = temp;
+printf("\nAfter swap a=%d b=%d",a,b);    
+return 0;  
+} 
