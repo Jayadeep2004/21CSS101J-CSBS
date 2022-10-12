78c61124ffbd3fe942507b3a665fd898efe96075
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-12 10:32:25 +0530

    Update To check whether the year is leap year.c
    output : RA2211042010005
    Enter a year: 2016
    2016 is a leap year.

--- To check whether the year is leap year.c
+++ To check whether the year is leap year.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
-int main() {
+int main()
+{
+   printf("RA2211042010005\n");
    int year;
    printf("Enter a year: ");
    scanf("%d", &year);
 
-   if (year % 400 == 0) {
-      printf("%d is a leap year.", year);
-   }
+     if (year % 400 == 0) {
+       printf("%d is a leap year.", year);
+ }
    
-   else if (year % 100 == 0) {
-      printf("%d is not a leap year.", year);
-   }
+     else if (year % 100 == 0) {
+        printf("%d is not a leap year.", year);
+ }
  
-   else if (year % 4 == 0) {
-      printf("%d is a leap year.", year);
-   }
+     else if (year % 4 == 0) {
+         printf("%d is a leap year.", year);
+ }
    
-   else {
-      printf("%d is not a leap year.", year);
-   }
+      else {
+          printf("%d is not a leap year.", year);
+  }
 
    return 0;
-}
+   }
