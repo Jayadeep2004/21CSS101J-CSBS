d30709890bc32bd326ce9d0ae7d92f4a44466124
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-12 10:18:51 +0530

    Update and rename Switchc to Switch.c
    output : RA2211042010005
    Sunday

---
+++ Switch.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+  int day = 2;
+  switch (day) {
+    case 1:
+          printf("Monday");
+      break;
+    case 2:
+          printf("Sunday");
+      break;
+  }}
