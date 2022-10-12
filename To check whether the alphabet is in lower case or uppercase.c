27901b96442e7dd99d102a955f624e0457f69fd0
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-12 10:23:35 +0530

    Update To check whether the alphabet is in lower case or uppercase.c
    output : RA2211042010005
    Enter the alphabet:a
    lower case

--- To check whether the alphabet is in lower case or uppercase.c
+++ To check whether the alphabet is in lower case or uppercase.c
@@ -2,21 +2,16 @@
 
 int main() 
 {
-    char ch; 
-    printf("Enter the alphabet:");
-
-    scanf("%c",&ch);
+  printf("RA2211042010005\n");
+      char ch; 
+     printf("Enter the alphabet:");
+  scanf("%c",&ch);
 
    if(ch>=65 && ch<=90)
+    printf("upper case");
 
-      printf("upper case");
-
-    else if(ch>=97 && ch<=122)
- 
-
+  else if(ch>=97 && ch<=122)
     printf("lower case");
-
-     
     else
      printf("Invalid input");
 return 0;
