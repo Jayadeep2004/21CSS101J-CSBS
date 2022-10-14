d5322040f9090676c10df7ae99d16f13993ff817
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-14 15:59:56 +0530

    Update to find factorial of a given number.c
    output : RA2211042010005
    Enter a positive integer: 8
    Factorial = 40320

--- to find factorial of a given number.c
+++ to find factorial of a given number.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main() 
 {
-    int n, i, factorial = 1;
-    printf("Enter a positive integer: ");
-    scanf("%d", &n);
+    printf("RA2211042010005\n");
+ int n, i, factorial = 1;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
 
-    for (i = 1; i <= n; ++i) 
-        factorial *= i;
+   for (i = 1; i <= n; ++i) 
+     factorial *= i;
 
-    printf("Factorial = %d", factorial);
-    return 0;
+      printf("Factorial = %d", factorial);
+      return 0;
 }
