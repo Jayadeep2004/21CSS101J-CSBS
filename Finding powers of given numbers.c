a66f7c8b2882a00cc7df5c229d27391e93dbbf07
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-14 16:24:24 +0530

    Update Finding powers of given numbers.c
    output : RA2211042010005
    Enter a base number: 4
    Enter an exponent: 4
    Answer = 256

--- Finding powers of given numbers.c
+++ Finding powers of given numbers.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main() {
-    int base, exp;
-    long double result = 1.0;
-    printf("RA2211042010005\n");
-    printf("Enter a base number: ");
+int main() 
+{
+ int base, exp;
+ long double result = 1.0;
+   printf("RA2211042010005\n");
+   printf("Enter a base number: ");
     scanf("%d", &base);
     printf("Enter an exponent: ");
     scanf("%d", &exp);
