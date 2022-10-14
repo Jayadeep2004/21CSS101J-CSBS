240657c6836240c31209d27eccf095839ad998ce
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-14 15:32:15 +0530

    Update radius of the circle.c
    output : RA2211042010005
    Enter radius of circle: 6
    Area of circle : 113.040001

--- radius of the circle.c
+++ radius of the circle.c
@@ -1,11 +1,12 @@
 #include<stdio.h> 
 int main() 
 { 
+  printf("RA2211042010005\n");
 int r; 
 float PI = 3.14, area; 
-printf("Enter radius of circle: "); 
-scanf("%d", &r); 
+ printf("Enter radius of circle: "); 
+ scanf("%d", &r); 
 area = PI * r * r; 
-printf("\nArea of circle : %f ", area); 
+ printf("\nArea of circle : %f ", area); 
 return 0; 
 } 
