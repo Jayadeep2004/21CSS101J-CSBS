6dde1436a2fe4caff6c267d305d7dad36e084e90
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-14 15:25:28 +0530

    Update post increment.c
    output : RA2211042010005
    The value returned after post increment of a is = 8
    The value of a now is = 9

--- post increment.c
+++ post increment.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 main()
-{
+{ 
+  printf("RA2211042010005\n");
 int a=8;
-printf("The value returned after post increment of a is = %d",a++);
-printf("\nThe value of a now is = %d",a);
+printf("The value returned after post increment of a is = %d\n",a++);
+printf("The value of a now is = %d",a);
 }
