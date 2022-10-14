d1f41c9c52f23dd64db57645e7c7a062e664b019
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-14 16:10:45 +0530

    Update Area of square.c
    output : RA2211042010005
    enter side of square: 4
    Area of the square : 16.000000

--- Area of square.c
+++ Area of square.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	float side,area;
-	printf("RA2211042010005\n");
-	printf("enter side of square: ");
-	scanf("%f",&side);
+float side,area;
+ printf("RA2211042010005\n");
+ printf("enter side of square: ");
+  scanf("%f",&side);
 	
-
-	area=(side*side);
-	printf("AOS: %f\n",area);
-	return 0;
+   area=(side*side);
+   printf("Area of the square : %f\n",area);
+return 0;
 }
