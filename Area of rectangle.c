f82d75f297f84f04906dddf990d9e162cbe676bc
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-10-14 16:08:07 +0530

    Update Area of rectangle.c
    output ; RA2211042010005
    enter length of rectangle: 6
    enter breadth of rectangle: 8
    Area of the rectangle: 48.000000

--- Area of rectangle.c
+++ Area of rectangle.c
@@ -3,12 +3,13 @@ int main()
 {
 float length,breadth,area;
   printf("RA2211042010005\n");
+  
   printf("enter length of rectangle: ");
   scanf("%f",&length);
   printf("enter breadth of rectangle: ");
   scanf("%f",&breadth);
 
-  area=(length*breadth);
-  printf("AOR: %f\n",area);
+  area = (length*breadth);
+  printf("Area of the rectangle: %f\n",area);
  return 0;
 }
