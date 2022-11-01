0fb40e417ed7fce2251039eb6960a5300c079783
Jayadeep2004 <113873626+Jayadeep2004@users.noreply.github.com>
2022-11-01 11:03:20 +0530

    Create while statement2.c
    output : 
    RA2211042010005
    Enter a number : 20
    20
    40
    60
    80
    100
    120
    140
    160
    180
    200
    220
    240
    260
    280
    300
    320
    340
    360
    380
    400

---
+++ while statement2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    printf("RA2211042010005\n");
+  int i=1, number = 0;
+  printf("Enter a number : ");
+  scanf("%d", &number);
+  while (i<=20) {
+      printf("%d\n",(number*i));
+      i++;
+  }
+  
+}
